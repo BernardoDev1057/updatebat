73ea6ee12dd5bc546408f3eb7d5bbc4e66e96145
BernardoDev1057 <bernardodev1057@gmail.com>
2024-07-11 23:13:43 -0300

    adicionado logica para uso do isql

--- script.bat
+++ script.bat
@@ -4,8 +4,12 @@ set "CONFIG_FILE=env.txt"
 :start
 REM Limpa a tela
 cls
+
+if exist resultado.txt (del resultado.txt)
+
 if exist "%CONFIG_FILE%"(
 	for /f "tokens=* usebackq" %%a in ("%%CONFIG_FILE") do (%%a)
+	isql %HOST%:%DATB% -u %USER% -P %PASS% -i sql.sql -o resultado.txt
 ) else (
 	echo Arquivo de configuração. '%CONFIG_FILE%' não encontrado.
 )
