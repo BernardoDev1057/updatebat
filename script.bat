8b184ba91ae6dfdb5fc19e23cae765676fb6565a
BernardoDev1057 <bernardodev1057@gmail.com>
2024-07-12 00:05:54 -0300

    esqueci os apostofros

--- script.bat
+++ script.bat
@@ -1,4 +1,5 @@
 @echo off
+
 set "CONFIG_FILE=env.txt"
 
 REM Limpa a tela
@@ -8,7 +9,7 @@ if exist resultado.txt (del resultado.txt)
 
 if exist "%CONFIG_FILE%"(
 	for /f "tokens=* usebackq" %%a in ("%%CONFIG_FILE") do (%%a)
-	isql %HOST%:%DATB% -u %USER% -P %PASS% -i sql.sql -o resultado.txt
+	isql "%HOST%":"%DATB%" -u "%USER%" -P "%PASS%" -i sql.sql -o resultado.txt
 ) else (
 	echo Arquivo de configuração. '%CONFIG_FILE%' não encontrado.
 )
