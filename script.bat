d19ed2bd9e29d144342bfa16b7f077916ce59cdc
BernardoDev1057 <bernardodev1057@gmail.com>
2024-07-11 22:54:02 -0300

    adicionado codigo para ler o env.txt

--- script.bat
+++ script.bat
@@ -1,7 +1,15 @@
 @echo off
+set "CONFIG_FILE=env.txt"
+
 :start
-REM Insira aqui o comando que deseja executar a cada 5 minutos
-echo Executando minha instrução...
+REM Limpa a tela
+cls
+if exist "%CONFIG_FILE%"(
+	for /f "tokens=* usebackq" %%a in ("%%CONFIG_FILE") do (%%a)
+) else (
+	echo Arquivo de configuração. '%CONFIG_FILE%' não encontrado.
+)
+
 
 REM Aguarda 5 minutos (300 segundos)
 ping 127.0.0.1 -n 301 > nul
